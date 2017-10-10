74d7594f12b0eddc952498c3a088f2aa7cce4667
Tony Peter <tpet93@gmail.com>
2017-10-10 12:23:58 +1000

    disable join + rx

--- src/lmic/config.h
+++ src/lmic/config.h
@@ -8,7 +8,7 @@
 //#define CFG_eu868 1
 #define CFG_us915 1
 #define CFG_au915 1
-//#define  DISABLE_RX  // remove receive window
+#define  DISABLE_RX  // remove receive window
 // This is the SX1272/SX1273 radio, which is also used on the HopeRF
 // RFM92 boards.
 //#define CFG_sx1272_radio 1
@@ -40,7 +40,7 @@
 #define LMIC_FAILURE_TO Serial
 
 // Uncomment this to disable all code related to joining
-//#define DISABLE_JOIN
+#define DISABLE_JOIN
 // Uncomment this to disable all code related to ping
 #define DISABLE_PING
 // Uncomment this to disable all code related to beacon tracking.
